ad17fc11e834dfd5fae238dd2c29ce168c3a4cd4
Atoyebi Adesoji O <29846777+adeatoye@users.noreply.github.com>
2021-05-29 20:36:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $(".burger-nav").on("click", function () {
+    $("nav ul.nav-links").toggleClass("open");
+  });
+});
+
+// let burger = document.querySelector(".burger-nav");
+
+// let nav = document.getElementsByClassName("nav nav-links");
+
+// console.log(nav);
+
+// burger.addEventListener('onclick', function(){
+
+
+//   nav.classList.toggle("open");
+
+// });
